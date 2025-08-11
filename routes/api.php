a48ee0a151e2b35e7d5d459ebebc8bb94796790d
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-11 14:21:11 +0300

    Refactor API routes to enhance authentication and verification endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::prefix('v1')->group(function () {
+
+    Route::prefix('auth')->group(function () {
+
+        Route::controller(AuthController::class)->group(function () {
+            Route::post('/login', 'login')->name('auth.login')->middleware('throttle:10,1');
+            Route::post('/register', 'register')->name('auth.register')->middleware('throttle:5,1');
+
+            Route::post('/verification/resend', 'resendVerificationCode')->name('auth.verification.resend')->middleware('throttle:5,1');
+            Route::post('/verification/verify', 'verifyEmail')->name('auth.verification.verify')->middleware('throttle:5,1');
+            Route::post('/password/reset/send', 'sendResetPasswordCode')->name('auth.password.reset.send')->middleware('throttle:5,1');
+            Route::post('/password/reset', 'resetPassword')->name('auth.password.reset')->middleware('throttle:5,1');
+
+            Route::middleware('auth:sanctum')->group(function () {
+                Route::post('/logout', 'logout')->name('auth.logout');
+            });
+        });
+    });
+});
