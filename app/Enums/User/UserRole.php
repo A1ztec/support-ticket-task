c6b94e4dbe008a0fc253b310cfe2fb33586b053b
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-11 13:09:44 +0300

    Add UserRole enum for defining user roles and their titles

---
+++ app/Enums/User/UserRole.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Enums\User;
+
+
+enum UserRole: string
+{
+    case USER = 'user';
+    case ADMIN = 'admin';
+
+
+
+
+    public function title(): string
+    {
+        return match ($this) {
+            self::USER => __('User'),
+            self::ADMIN => __('Admin'),
+        };
+    }
+
+
+    public function options(): array
+    {
+        return collect(self::cases())->mapWithKeys(fn($item) => [$item->value => $item->title()])->toArray();
+    }
+}
