e60a9ed959aa35a5fa94776212e88138db0dba7f
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-12 00:03:23 +0300

    Refactor error response handling in Handler to utilize ApiStatus enums for HTTP status codes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use App\Enums\System\ApiStatus;
 use App\Traits\ApiResponseTrait;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
@@ -22,26 +23,26 @@ class Handler extends ExceptionHandler
             if ($e instanceof ModelNotFoundException) {
                 return $this->errorResponse(
                     __('Resource not found.'),
-                    Response::HTTP_NOT_FOUND
+                    ApiStatus::NOT_FOUND->httpCode()
                 );
             }
 
             if ($e instanceof AuthorizationException) {
                 return $this->errorResponse(
                     __('You are not authorized to perform this action.'),
-                    Response::HTTP_FORBIDDEN
+                    ApiStatus::UNAUTHORIZED->httpCode()
                 );
             }
 
             if ($e instanceof ValidationException) {
                 return $this->errorResponse(
                     __('Validation failed.'),
-                    Response::HTTP_UNPROCESSABLE_ENTITY,
+                    ApiStatus::VALIDATION_FAILED->httpCode(),
                     $e->errors()
                 );
             }
 
-            
+
             Log::error('Unexpected error: ' . $e->getMessage(), [
                 'exception' => get_class($e),
                 'file' => $e->getFile(),
@@ -54,7 +55,7 @@ class Handler extends ExceptionHandler
 
             return $this->errorResponse(
                 __('Something went wrong.'),
-                Response::HTTP_INTERNAL_SERVER_ERROR
+                ApiStatus::ERROR->httpCode()
             );
         }
 
