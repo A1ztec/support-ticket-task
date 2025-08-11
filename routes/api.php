ebfefff7be1c801c7e84b8f1a5ca3d56c345fc6d
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-11 18:17:06 +0300

    Add ticket routes for authenticated users in API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TicketController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -16,7 +17,6 @@ Route::prefix('v1')->group(function () {
         Route::controller(AuthController::class)->group(function () {
             Route::post('/login', 'login')->name('auth.login')->middleware('throttle:10,1');
             Route::post('/register', 'register')->name('auth.register')->middleware('throttle:5,1');
-
             Route::post('/verification/resend', 'resendVerificationCode')->name('auth.verification.resend')->middleware('throttle:5,1');
             Route::post('/verification/verify', 'verifyEmail')->name('auth.verification.verify')->middleware('throttle:5,1');
             Route::post('/password/reset/send', 'sendResetPasswordCode')->name('auth.password.reset.send')->middleware('throttle:5,1');
@@ -27,4 +27,18 @@ Route::prefix('v1')->group(function () {
             });
         });
     });
+
+
+    Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+
+        Route::prefix('tickets')->group(function () {
+            Route::get('/', [TicketController::class, 'listTickets']);
+            Route::post('/', [TicketController::class, 'createTicket']);
+            Route::get('/{ticket}', [TicketController::class, 'showTicket']);
+            Route::post('/reply', [TicketController::class, 'reply']);
+            Route::get('/{ticket}/download-attachment', [TicketController::class, 'downloadAttachment'])
+                ->name('tickets.downloadAttachment');
+        });
+    });
 });
+
