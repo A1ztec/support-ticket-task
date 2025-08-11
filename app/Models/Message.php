375961ab7b9b4445d63d558842a6f3b7bcb5e108
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-11 13:10:20 +0300

    Refactor Message model to define fillable attributes and relationships

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    protected $fillable = [
+        'ticket_id',
+        'user_id',
+        'message',
+        'attachment',
+    ];
+
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
