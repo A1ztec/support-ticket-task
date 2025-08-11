12eb8c404a5cad3fa58a2c19a986f3bc7dc90e84
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-11 13:10:25 +0300

    Refactor Ticket model to define fillable attributes and relationships

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use App\Enums\Ticket\TicketStatus;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $fillable = [
+        'subject',
+        'message',
+        'user_id',
+        'status',
+        'attachment',
+    ];
+
+
+    protected function casts(): array
+    {
+        return [
+            'status' => TicketStatus::class,
+        ];
+    }
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
