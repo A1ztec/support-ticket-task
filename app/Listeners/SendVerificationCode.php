21afb3ed2df8af7bd1a95d76b674926e043a9beb
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-11 14:20:35 +0300

    Add SendVerificationCode listener to handle user registration and send verification emails

---
+++ app/Listeners/SendVerificationCode.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserRegistered;
+use App\Mail\VerificationCodeMail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+
+class SendVerificationCode
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(UserRegistered $event): void
+    {
+        try {
+
+            Mail::to($event->user->email)->queue(new VerificationCodeMail($event->verificationCode));
+            Log::channel('email')->info('Sent verification code to: ' . $event->user->email);
+        } catch (\Exception $e) {
+
+            Log::error('Failed to send verification code: ' . $e->getMessage());
+        }
+    }
+}
