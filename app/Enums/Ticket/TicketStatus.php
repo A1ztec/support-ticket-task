d6d471b30cb0943a6c8b3aed60814b4f40a3b0cf
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-11 13:09:39 +0300

    Add TicketStatus enum for managing ticket states

---
+++ app/Enums/Ticket/TicketStatus.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Enums\Ticket;
+
+
+enum TicketStatus: string
+{
+    case OPEN = 'open';
+    case IN_PROGRESS = 'in_progress';
+    case CLOSED = 'closed';
+
+
+
+public function title() : string
+    {
+        return match ($this) {
+            self::OPEN => __('Open'),
+            self::IN_PROGRESS => __('In Progress'),
+            self::CLOSED => __('Closed'),
+        };
+    }
+
+    public function color(): string
+    {
+        return match ($this) {
+            self::OPEN => 'blue',
+            self::IN_PROGRESS => 'yellow',
+            self::CLOSED => 'green',
+        };
+    }
+
+
+    public function options() : array
+    {
+        return collect(self::cases())->mapWithKeys(fn($item) => [$item->value => $item->title()])->toArray();
+    }
+
+}
