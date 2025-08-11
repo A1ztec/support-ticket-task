2e705c4b4c5124127fa31763d4e8569505ba4d3a
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-12 02:53:33 +0300

    Add admin authentication routes and middleware for access control

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
 });
+
+    Route::prefix('admin')->name('admin.')->group(function()
+    {
+        Route::prefix('auth')->name('auth.')->group(function () {
+            Route::get('login', [AuthController::class, 'showLogin'])->name('login');
+            Route::post('login', [AuthController::class, 'login'])->name('login.post');
+            Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+        });
+
+        Route::middleware(['auth' , 'admin'])->group(function () {
+
+        });
+    });
