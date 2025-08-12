3e336403047ed3b7a7b2a8da78219559edc36efb
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-12 05:50:14 +0300

    Implement ShouldQueue interface in TicketRepliedNotification class

--- app/Notifications/TicketRepliedNotification.php
+++ app/Notifications/TicketRepliedNotification.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class TicketRepliedNotification extends Notification
+class TicketRepliedNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -34,7 +34,7 @@ class TicketRepliedNotification extends Notification
     /**
      * Get the mail representation of the notification.
      */
-    public function toMail(object $notifiable) : TicketReplyMail
+    public function toMail(object $notifiable): TicketReplyMail
     {
         return new TicketReplyMail($this->ticket);
     }
