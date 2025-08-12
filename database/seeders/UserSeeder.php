c37d7afd93d09241e2e0711a402bad6bb809783b
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-12 03:57:11 +0300

    Add UserSeeder to populate initial user data in the database

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'Admin User',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('password'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@example.com',
+                'password' => bcrypt('password'),
+                'role' => 'user',
+                'email_verified_at' => now(),
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
