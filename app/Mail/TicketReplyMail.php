8f92260cdb81522609e209d383de3a39cb8e28f4
Tarek Mohamed <A1ztec1@gmail.com>
2025-08-12 05:50:09 +0300

    Implement ShouldQueue interface in TicketReplyMail class

--- app/Mail/TicketReplyMail.php
+++ app/Mail/TicketReplyMail.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class TicketReplyMail extends Mailable
+class TicketReplyMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
