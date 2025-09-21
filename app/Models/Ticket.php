a4aba981f39c4df1afc8da07f39b56125d3ea468
Tarek Mohamed <A1ztec1@gmail.com>
2025-09-21 14:03:49 +0300

    Add HasFactory trait to Ticket model for factory support

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use App\Enums\Ticket\TicketStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'subject',
         'message',
